ee9bea37a3259f5abb47e97bdb4aa41d13436eec
“ShomaShirai” <takosu9314@gmail.com>
2025-10-27 12:07:02 +0900

    ストレージのセキュリティーコマンドを実行

--- supabase/migrations/007_init_storage.sql
+++ supabase/migrations/007_init_storage.sql
@@ -7,3 +7,27 @@ insert into storage.buckets
     (id, name, public)
 VALUES
     ('maps', 'Maps Bucket', true);
+
+CREATE POLICY "public can select in abstracts and maps"
+    ON storage.objects
+    FOR SELECT
+    TO public
+    USING (bucket_id IN ('abstracts', 'maps'));
+
+CREATE POLICY "public can insert in abstracts and maps"
+    ON storage.objects
+    FOR INSERT
+    TO public
+    USING (bucket_id IN ('abstracts', 'maps'));
+
+CREATE POLICY "public can update in abstracts and maps"
+    ON storage.objects
+    FOR UPDATE
+    TO public
+    USING (bucket_id IN ('abstracts', 'maps'));
+
+CREATE POLICY "public can delete in abstracts and maps"
+    ON storage.objects
+    FOR DELETE
+    TO public
+    USING (bucket_id IN ('abstracts', 'maps'));
