8f57cd9dcf2fabfceb84eaa5c38dcaf2244c51b8
“ShomaShirai” <takosu9314@gmail.com>
2025-10-27 11:40:57 +0900

    バケットの追加に関しても，migrationsディレクトリに追加

---
+++ supabase/migrations/007_init_storage.sql
@@ -0,0 +1,9 @@
+insert into storage.buckets
+    (id, name, public)
+VALUES
+    ('abstracts', 'Abstracts Bucket', true)
+
+insert into storage.buckets
+    (id, name, public)
+VALUES
+    ('maps', 'Maps Bucket', true);
